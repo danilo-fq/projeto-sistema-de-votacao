449087d79d73281c62b3c28f19cffbb8854fecfa
Danilo Queiroz <danilof.queiroz1@gmail.com>
2026-02-19 15:28:35 -0300

    feat: create abstract Pessoa class

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -6,6 +6,14 @@ package com.betrybe.sistemadevotacao;
  *
  * @author Danilo
  * */
-public class Pessoa {
-  
+public abstract class Pessoa {
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
