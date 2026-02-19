8ba1ae87049f597e0423424f4c88cf22b56fe67d
Danilo Queiroz <danilof.queiroz1@gmail.com>
2026-02-19 16:29:51 -0300

    feat: create PessoaEleitora class, implements more description on javadoc class

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -2,7 +2,32 @@ package com.betrybe.sistemadevotacao;
 
 /**
  * Representa uma pessoa apta a exercer o direito de voto no sistema.
- * */
-public class PessoaEleitora{
+ * <p>
+ *  Esta classe estende a base comum {@link Pessoa}, adicionando um atributo
+ *  {@code cpf} para identificação única da pessoa eleitora.
+ * </p>
+ *
+ * @author Danilo
+ * @see Pessoa
+ */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  /**
+   * Inicializa uma nova instância de uma pessoa eleitora com nome e CPF.
+   * @param nome O nome completo da pessoa eleitora.
+   * @param cpf O CPF da pessoa eleitora para validação de voto.
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
