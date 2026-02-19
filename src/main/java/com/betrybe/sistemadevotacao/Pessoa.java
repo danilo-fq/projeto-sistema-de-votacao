c58c83fae1196d91d0e350a1e953ceec7f6db5c7
Danilo Queiroz <danilof.queiroz1@gmail.com>
2026-02-19 15:45:23 -0300

    feat: include more description on javadoc

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -4,6 +4,12 @@ package com.betrybe.sistemadevotacao;
  * Classe abstrata que serve como base para todos os participantes do
  * sistema de votação.
  *
+ * <p>
+ *  Esta classe define a estrutura comum de identificação para os diferentes papéis
+ *  no processo eleitoral, sendo herdada por candidatos e eleitores. Por ser abstrata,
+ *  não permite instanciamento direto, funcionando estritamente como um molde.
+ * </p>
+ *
  * @author Danilo
  * */
 public abstract class Pessoa {
